253b4d2aeb70888d8ce9718ed1b22ae5e20602f7
rohitjose <rohitjose@gmail.com>
2014-08-07 06:34:40 +0530

    Revised code for second table

--- DBtodocumentDB/MongoInsert.py
+++ DBtodocumentDB/MongoInsert.py
@@ -1,7 +1,7 @@
 __author__ = 'rohit'
 from pymongo import MongoClient
 
-def construct_record(rows):
+def construct_project_record(rows):
     '''Constructs the JSON structure for the MongoDB document'''
     dict = {}
     dict_list = []
@@ -31,6 +31,41 @@ def insert_record(dict_record):
     '''Inserts a record into MongoDB'''
     connection = MongoClient("localhost")#establishes connection
     db = connection.COMPANY.projects#connects to the collection
+    #inserts the record
+    db.insert(dict_record)
+    # close the connection to MongoDB
+    connection.close()
+
+def construct_department_record(records):
+    '''Constructs the JSON structure for the MongoDB document'''
+    dict = {}
+    dict_list = []
+    for record in records:
+        d_number,d_name,l_name,location = record
+
+        if len(dict)==0:#initial record
+            dict['dnumber'] = d_number
+            dict['dname'] = d_name
+            dict['lname'] = l_name
+            dict['location'] = [location]
+        elif dict['dnumber']!=d_number:#new record
+            dict_list.append(dict)
+            dict = {}
+            dict['dnumber'] = d_number
+            dict['dname'] = d_name
+            dict['lname'] = l_name
+            dict['location'] = [location]
+        elif dict['dnumber']== d_number:#new location
+            location_list = dict['location']
+            location_list.append(location)
+            dict['location'] = location_list
+
+    return dict_list#returns the target JSON structure abstracted in a list
+
+def insert_record_department(dict_record):
+    '''Inserts a record into MongoDB'''
+    connection = MongoClient("localhost")#establishes connection
+    db = connection.COMPANY.department#connects to the collection
     #inserts the record
     db.insert(dict_record)
     # close the connection to MongoDB
--- DBtodocumentDB/__init__.py
+++ DBtodocumentDB/__init__.py
@@ -2,14 +2,30 @@ __author__ = 'rohit'
 import MongoInsert
 import AccessSQL
 
-if __name__=='__main__':
+def projects():
     #retrieves the rows of the MySQL DB
     rows = AccessSQL.accessSQL("select p.pnumber,p.pname, d.dname, e.fname,e.lname,w.hours \
     from PROJECT p , DEPARTMENT d , WORKS_ON w, EMPLOYEE e \
     where p.dnum=d.dnumber and p.pnumber=w.pno and w.essn=e.ssn \
     order by p.pnumber")
     #Constructs the target JSON structure
-    dicts = MongoInsert.construct_record(rows)
+    dicts = MongoInsert.construct_project_record(rows)
     # Loop for insertion into Mongo DB
     for item in dicts:
         MongoInsert.insert_record(item)
+
+def department():
+    rows = AccessSQL.accessSQL("SELECT D.Dnumber,D.DName, E.Lname, L.Dlocation \
+    FROM DEPARTMENT D, EMPLOYEE E, DEPT_LOCATIONS L \
+    WHERE D.Mgr_ssn = E.Ssn AND  D.Dnumber=L.Dnumber \
+    ORDER BY D.Dnumber")
+
+    dicts = MongoInsert.construct_department_record(rows)
+
+    for item in dicts:
+        MongoInsert.insert_record_department(item)
+
+
+if __name__=='__main__':
+    department()
+
